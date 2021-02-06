9ecd2af1e2ab6cf709d97a06fe9a236cede46382
AleFF187 <1871847@gmail.com>
2021-02-06 18:44:27 +0200

    сделал слайдер, стилизовал управление, наполнение не делал

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
+//! слайдер
 
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    // dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+// *******************************************************
+
+//! табы
 
 // получаем все кнопки
 const tabButtons = document.querySelectorAll('.tab__button');
